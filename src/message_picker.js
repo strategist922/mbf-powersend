9f874fbf5b85752e316c7fd02189370e37fb1df3
Davide Aversa <thek3nger@gmail.com>
2017-06-14 13:08:47 +0200

    Fix 	his reference bug.

--- src/message_picker.js
+++ src/message_picker.js
@@ -1,16 +1,16 @@
 let fs = require('fs')
 
 function MessagePicker(message_corpus_file) {
-    this.message_corpus = JSON.parse(fs.readFileSync(message_corpus, 'utf8'));
+    this.message_corpus = JSON.parse(fs.readFileSync(message_corpus_file, 'utf8'));
 }
 
 MessagePicker.prototype.getMessage = function getMessage(category, id) {
-    return message_corpus[category][id];
+    return this.message_corpus[category][id];
 }
 
 MessagePicker.prototype.pickMessage = function pickMessage(category) {
     let i = Math.floor(Math.random()*message_corpus[category].length)
-    return message_corpus[category][i];
+    return this.message_corpus[category][i];
 } 
 
 module.exports = MessagePicker;
