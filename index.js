62a99b5ae346362b4840b921f8269afc301d19c1
Davide Aversa <thek3nger@gmail.com>
2017-06-14 18:40:27 +0200

    Set index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let MessagePicker = require('./src/message_picker');
+let PowerSend = require('./src/powersend');
+
+module.exports.MessagePicker = MessagePicker;
+module.exports.PowerSend = PowerSend;
