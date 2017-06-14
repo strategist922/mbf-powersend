863829c451ec12dccf33a6898b4cafd5da79be7b
Davide Aversa <thek3nger@gmail.com>
2017-06-14 13:03:14 +0200

    Add message picker module

---
+++ src/message_picker.js
@@ -0,0 +1,16 @@
+let fs = require('fs')
+
+function MessagePicker(message_corpus_file) {
+    this.message_corpus = JSON.parse(fs.readFileSync(message_corpus, 'utf8'));
+}
+
+MessagePicker.prototype.getMessage = function getMessage(category, id) {
+    return message_corpus[category][id];
+}
+
+MessagePicker.prototype.pickMessage = function pickMessage(category) {
+    let i = Math.floor(Math.random()*message_corpus[category].length)
+    return message_corpus[category][i];
+} 
+
+module.exports = MessagePicker;
